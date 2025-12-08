2379ee8bdec644082d7211489bc5d70a11ec0b49
Breno Rodrigues <brenocrs@gmail.com>
2025-12-08 09:44:59 -0300

    fix: packages fix

--- runs/01-packages.sh
+++ runs/01-packages.sh
@@ -4,8 +4,20 @@ set -e
 
 echo "Installing packages..."
 
+# Official repos
 paru -S --noconfirm --needed - < packages/base.txt
 paru -S --noconfirm --needed - < packages/desktop.txt
 paru -S --noconfirm --needed - < packages/apps.txt
+
+# AUR packages (one by one)
+echo "Installing AUR packages..."
+paru -S --noconfirm --needed kanata-bin
+paru -S --noconfirm --needed cursor-bin
+paru -S --noconfirm --needed bun-bin
+paru -S --noconfirm --needed 1password
+paru -S --noconfirm --needed 1password-cli
+paru -S --noconfirm --needed spotify-launcher
+paru -S --noconfirm --needed youtube-desktop
+paru -S --noconfirm --needed xpadneo-dkms
 
 echo "Packages installed!"
